27d050b3bb9f8fd35cf0fc3e09476e5580d804bb
John Lockwood <elitepropertiesbroker@gmail.com>
2019-02-08 08:08:46 -0500

    Begin reading config.toml using viper

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module "https://github.com/CodeSolid/gowritego"
+
+require github.com/spf13/viper v1.3.1
---
+++ gowrite.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() 
+	if err != nil { 
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	fmt.Println(viper.Get("greeting"))
+}
